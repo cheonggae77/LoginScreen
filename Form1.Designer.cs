a6dde578020c1526f7d5b1ff2019e64910174b77
SANGHUN CHOI <cheonggae77@gmail.com>
2026-04-02 16:39:20 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace LoginScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblAppLogin = new Label();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblAppLogin
+            // 
+            lblAppLogin.AutoSize = true;
+            lblAppLogin.Font = new Font("맑은 고딕", 49.8000031F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblAppLogin.ForeColor = Color.Sienna;
+            lblAppLogin.Location = new Point(173, 48);
+            lblAppLogin.Name = "lblAppLogin";
+            lblAppLogin.Size = new Size(269, 112);
+            lblAppLogin.TabIndex = 0;
+            lblAppLogin.Text = "Login";
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("맑은 고딕", 25F);
+            txtID.ForeColor = Color.Silver;
+            txtID.Location = new Point(67, 200);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(480, 63);
+            txtID.TabIndex = 2;
+            txtID.Text = "아이디";
+            txtID.Enter += txtID_Enter;
+            txtID.Leave += txtID_Leave;
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("맑은 고딕", 25F);
+            txtPW.ForeColor = Color.Silver;
+            txtPW.Location = new Point(67, 305);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(480, 63);
+            txtPW.TabIndex = 3;
+            txtPW.Text = "패스워드";
+            txtPW.Enter += txtPW_Enter;
+            txtPW.Leave += txtPW_Leave;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = SystemColors.ActiveCaption;
+            btnLogin.Font = new Font("맑은 고딕", 30F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnLogin.ForeColor = Color.Blue;
+            btnLogin.Location = new Point(189, 429);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(236, 94);
+            btnLogin.TabIndex = 1;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Moccasin;
+            ClientSize = new Size(614, 587);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(lblAppLogin);
+            Name = "Form1";
+            Text = "Login Screen";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblAppLogin;
+        private TextBox txtID;
+        private TextBox txtPW;
+        private Button btnLogin;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# (C# 코딩) 로그인 화면
+## 개요
+- C# 프로그래밍 학습
+- 1줄 소개: 아이디와 비밀번호를 입력해 로그인하는 간단한 프로그램
+- 사용한 플랫폼:
+- C#, .NET Windows Forms, Visual Studio, GitHub
+- 사용한 컨트롤:
+- Label, TextBox, Button
+- 사용한 기술과 구현한 기능:
